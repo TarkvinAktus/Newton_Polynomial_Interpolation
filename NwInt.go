bcb4749bbef3d2da150f205ec28ae25ac965bd3b
Ilya <aleshin1196@gmail.com>
2019-08-28 03:42:40 +0300

    Go serv and canvas tool

--- NwInt.go
+++ NwInt.go
@@ -1,4 +1,5 @@
 package main
+
 //Посроение интерполяционного многочлена Ньютона
 import (
 	"fmt"
@@ -10,25 +11,25 @@ import (
 //fa(Xi,Xi+1) = f(Xi+1)-f(Xi) / Xi+1 - Xi
 //fa(X1,X2,X3) = fa(X2,X3) - fa(X1,X2) / X3 - X1
 //И т.д.
-//На вход i - номер коэффициента; div_depth - шаг деления
+//На вход i - номер коэффициента; divDepth - шаг деления
 //x - иксы, fx - игреки
-func fa(i int, div_depth int, x []int, fx []int) float64 {
+func fa(i int, divDepth int, x []int, fx []int) float64 {
 	var result float64
 	var div float64
 	if i == 1 {
-		result = float64( fx[i+div_depth] - fx[i+div_depth-1] )
-		div = float64(x[i+div_depth] - x[i+div_depth-1])
+		result = float64(fx[i+divDepth] - fx[i+divDepth-1])
+		div = float64(x[i+divDepth] - x[i+divDepth-1])
 		if div != 0 {
 			result /= div
 		} else {
 			result = 0
 		}
 	} else {
-		result = (fa(i-1,div_depth+1,x,fx) - fa(i-1,div_depth,x,fx) )
-		div= float64(x[i+div_depth] - x[0 + div_depth])
+		result = (fa(i-1, divDepth+1, x, fx) - fa(i-1, divDepth, x, fx))
+		div = float64(x[i+divDepth] - x[0+divDepth])
 		if div != 0 {
 			result /= div
-			result = toFixed(result,2)
+			result = toFixed(result, 2)
 		} else {
 			result = 0
 		}
@@ -36,56 +37,53 @@ func fa(i int, div_depth int, x []int, fx []int) float64 {
 	return result
 }
 
-
-func polynom(x []int,y []int, x_point int) float64{
-	var Polynom_sum float64
+func polynom(x []int, y []int, Xpoint int) float64 {
+	var PolynomSum float64
 	var buf float64
 
-	Polynom_sum += float64(y[0])
+	PolynomSum += float64(y[0])
 
-	for i := 1;i<(len(x)); i++ {
-		buf = fa(i,0,x,y) 
-		for j := 0;j<i; j++ {
-			buf *= float64(x_point-x[j]) 
+	for i := 1; i < (len(x)); i++ {
+		buf = fa(i, 0, x, y)
+		for j := 0; j < i; j++ {
+			buf *= float64(Xpoint - x[j])
 		}
-		Polynom_sum += buf
+		PolynomSum += buf
 	}
-	return math.Round(Polynom_sum)
+	return math.Round(PolynomSum)
 }
 
-
-func check(x int) float64{
+func check(x int) float64 {
 	var buf float64
-	buf = (float64(-17)/float64(6))*(math.Pow(float64(x),3)) + (math.Pow(float64(x),2)*12.5) - (float64(38)/float64(3) * float64(x)) - 2
-	return math.Round(buf) 
+	buf = (float64(-17)/float64(6))*(math.Pow(float64(x), 3)) + (math.Pow(float64(x), 2) * 12.5) - (float64(38) / float64(3) * float64(x)) - 2
+	return math.Round(buf)
 
 }
 
-func main() {
+func test() {
 
-	x := []int{0,1,2,3}
-	fx := []int{-2,-5,0,-4}
-/*
-	fmt.Println("fa(10) = ",fa(1,0,x,fx))
-	fmt.Println("fa(11) = ",fa(1,1,x,fx))
-	fmt.Println("fa(12) = ",fa(1,2,x,fx))
-	fmt.Println("fa(20) = ",fa(2,0,x,fx))
-	fmt.Println("fa(21) = ",fa(2,1,x,fx))
-	fmt.Println("fa(30) = ",fa(3,0,x,fx))
-*/
+	x := []int{0, 1, 2, 3}
+	fx := []int{-2, -5, 0, -4}
+	/*
+		fmt.Println("fa(10) = ",fa(1,0,x,fx))
+		fmt.Println("fa(11) = ",fa(1,1,x,fx))
+		fmt.Println("fa(12) = ",fa(1,2,x,fx))
+		fmt.Println("fa(20) = ",fa(2,0,x,fx))
+		fmt.Println("fa(21) = ",fa(2,1,x,fx))
+		fmt.Println("fa(30) = ",fa(3,0,x,fx))
+	*/
 
-	
-	fmt.Println("___check 0 ",check(0))
-	fmt.Println("___check 1 = ",check(1))
-	fmt.Println("___check 2 = ",check(2))
-	fmt.Println("___check 3 = ",check(3))
-	fmt.Println("___check 3 = ",check(4))
-	
-	fmt.Println("polynom 0 = ",polynom(x,fx,0))
-	fmt.Println("polynom 1 = ",polynom(x,fx,1))
-	fmt.Println("polynom 2 = ",polynom(x,fx,2))
-	fmt.Println("polynom 3 = ",polynom(x,fx,3))
-	fmt.Println("polynom 4 = ",polynom(x,fx,4))
+	fmt.Println("___check 0 ", check(0))
+	fmt.Println("___check 1 = ", check(1))
+	fmt.Println("___check 2 = ", check(2))
+	fmt.Println("___check 3 = ", check(3))
+	fmt.Println("___check 3 = ", check(4))
+
+	fmt.Println("polynom 0 = ", polynom(x, fx, 0))
+	fmt.Println("polynom 1 = ", polynom(x, fx, 1))
+	fmt.Println("polynom 2 = ", polynom(x, fx, 2))
+	fmt.Println("polynom 3 = ", polynom(x, fx, 3))
+	fmt.Println("polynom 4 = ", polynom(x, fx, 4))
 
 	var scan string
 	fmt.Scan(&scan)
---
+++ ServerJSON.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"math"
+	"net/http"
+	"net/url"
+)
+
+//Resp ОТВЕТНЫЕ ТОЧКИ
+type Resp struct {
+	X []int `json:"x"`
+	Y []int `json:"y"`
+}
+
+func startServer() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Printf("REQUEST -------- HOST - %s METHOD - %s Params - %s \n", r.Host, r.Method, r.URL.Query())
+
+		myX := r.URL.Query().Get("x")
+		myY := r.URL.Query().Get("y")
+		fmt.Println("x = ", myX, " y = ", myY)
+
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Content-Type", "application/json")
+		var res Resp
+
+		res.X = make([]int, 10)
+		res.Y = make([]int, 10)
+
+		for i := 0; i < 10; i++ {
+			res.X[i] = int(math.Pow(2, float64(i)))
+			res.Y[i] = i
+		}
+
+		resJSON, err := json.Marshal(res)
+		if err != nil {
+			fmt.Printf("Error: %s", err)
+		}
+
+		w.Write(resJSON)
+	})
+	/*
+		http.HandleFunc("/raw_body", func(w http.ResponseWriter, r *http.Request) {
+			body, err := ioutil.ReadAll(r.Body)
+			defer r.Body.Close() // важный пункт!
+			if err != nil {
+				http.Error(w, err.Error(), 500)
+				return
+			}
+			fmt.Fprintf(w, "postHandler: raw body %s\n", string(body))
+		})
+	*/
+	fmt.Println("starting server at :9000")
+	http.ListenAndServe(":9000", nil)
+}
+
+func runGetFullReq() {
+
+	req := &http.Request{
+		Method: http.MethodGet,
+		Header: http.Header{
+			"User-Agent": {"Notebook/golang"},
+		},
+	}
+
+	req.URL, _ = url.Parse("http://192.168.0.98:9000/?msg=1234556789")
+	req.URL.Query().Add("param", "param1")
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		fmt.Println("error happend", err)
+		return
+	}
+	defer resp.Body.Close() // важный пункт!
+
+	respBody, err := ioutil.ReadAll(resp.Body)
+
+	fmt.Printf("testGetFullReq resp %#v\n\n\n", string(respBody))
+}
+
+func main() {
+	startServer()
+}
